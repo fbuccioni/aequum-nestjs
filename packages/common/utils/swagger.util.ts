acdcc9edd1d623a07ce60ca9cb8511925c66eede
Felipe Buccioni <falcacibar@gmail.com>
2025-06-15 02:54:34 -0400

    improve(common): Improved doc for `patchDocumentViaSwaggerExplorer` function

--- packages/common/utils/swagger.util.ts
+++ packages/common/utils/swagger.util.ts
@@ -13,10 +13,53 @@ type GenerateDenormalizedDocument = (
 ) => any;
 
 /**
- * Patch the document by monkey patching the `SwaggerExplorer` class in the
- * method `generateDenormalizedDocument`.
+ * Patch the document by monkey patching the `SwaggerExplorer`
+ * class in the method `generateDenormalizedDocument`
+ *
+ * **NOTE**: This function musb be called once
  *
  * @param generateDenormalizedDocument
+ * @example ```
+ * // Excample to patch a route param
+ *
+ * // Controller file
+ * @ApiTags('Data set')
+ * @Controller('data')
+ * export class DataController {
+ *     @ApiParam({
+ *         name: 'path',
+ *         description: 'Path for the dataset',
+ *         required: true,
+ *         allowReserved: true,
+ *         schema: { type: 'string', pattern: '^[a-z0-9_/-]+$' },
+ *     })
+ *     @ApiResponse({
+ *         status: 200,
+ *         description: `The data set data retrieved successfully.`,
+ *     })
+ *     @Get('*')
+ *     async dataRoute(
+ *         @Param('*') path: string,
+ *     ): Promise<{ data: JSON }> {
+ *         // ...
+ *     }
+ * }
+ *
+ * // Swagger patch file to load, the function must be
+ * // called once
+ * import { swagger } from '@aequum/nestjs-common/utils';
+ *
+ * swagger.patchDocumentViaSwaggerExplorer(function (document, metatype) {
+ *   if (metatype.name === 'DataController') {
+ *       document[0].root.path = document[0].root.path.replace(
+ *           /\*$/,
+ *           '{path}',
+ *       );
+ *       document[0].root.parameters =
+ *           document[0].root.parameters.filter((p) => p.name !== '*');
+ *
+ *   return document;
+ * });
  */
 export function patchDocumentViaSwaggerExplorer(generateDenormalizedDocument: GenerateDenormalizedDocument) {
     // @ts-ignore
@@ -41,5 +84,6 @@ export function patchDocumentViaSwaggerExplorer(generateDenormalizedDocument: Ge
 export function authModName(auth: string) {
     if (auth === 'jwt') return 'Bearer';
     if (auth === 'oauth2') return 'OAuth2';
+    if (auth === 'oauth') return 'OAuth2';
     return auth[0].toUpperCase() + auth.slice(1);
 }
