1849243ac8e522e4f7e5499c63e60e9515920d98
Felipe Buccioni <falcacibar@gmail.com>
2025-06-15 03:22:48 -0400

    improve(paginate-common,uniform-data): Improved DTOs factory classes for typing and naming of the return class

--- packages/paginate-common/dtos/paginated.dto.ts
+++ packages/paginate-common/dtos/paginated.dto.ts
@@ -1,34 +1,68 @@
 import { ApiProperty, getSchemaPath } from '@nestjs/swagger';
-import { PaginateResult } from "@aequum/paginate-common";
 import { ClassConstructor } from '@aequum/types';
 
-import { PaginatorDto } from "./paginator.dto";
+import { PaginatorDto as PaginatorDtoClass } from "./paginator.dto";
 
 
 /**
- * Class to create a DTO for pagination with a custom filter if needed.
+ * Factory class to create a DTO for pagination with a custom filter
+ * if needed.
  *
- * @param type - DTO class to paginate
- * @example ```typescript
- * class ModelPaginatedListDto extends PaginatedDto(ModelDto) {}
- * ```
+ * @typeParam Dto - Type from `dto` argument
+ * @typeParam PaginatorDto - Type from `paginatorDto` argument
+ * @typeParam DtoType - Instance type from `dto` argument if it is an
+ * array extract the type from the first element.
+ *
+ * @param dto - DTO class to paginate
+ * @param paginatorDto - Paginator DTO class, defaults to
+ * `PaginatorDtoClass`
+ *
  * @constructor
  */
-export function PaginatedDto<T extends ClassConstructor>(type: T) {
-    class PaginatedDto implements PaginateResult<T>{
+export function PaginatedDto<
+    Dto extends ClassConstructor | [ ClassConstructor ],
+    PaginatorDto extends ClassConstructor = typeof PaginatorDtoClass,
+    DtoType = (
+        // tuple
+        Dto extends [ infer U extends ClassConstructor ]
+        ? InstanceType<U>
+        : (
+            // single class
+            Dto extends ClassConstructor
+            ? InstanceType<Dto>
+            : (
+                // array of class
+                Dto extends Array<infer U extends ClassConstructor>
+                ? InstanceType<U>[]
+                : never
+            )
+        )
+    )
+>(
+    dto: Dto,
+    paginatorDto: PaginatorDto | typeof PaginatorDtoClass = PaginatorDtoClass
+) {
+    const realType = Array.isArray(dto) ? dto[0] : dto as Function;
+
+    class PaginatedDto$<D, P> {
         @ApiProperty({
             description: 'Result data',
             type: 'array',
-            items:  { $ref: getSchemaPath(type) },
+            items: { $ref: getSchemaPath(realType) },
         })
-        data: T[];
+        data: D[];
 
         @ApiProperty({
             description: 'Paginator info',
-            type: PaginatorDto
+            type: paginatorDto
         })
-        paginator: PaginatorDto
+        paginator: P
     }
 
-    return PaginatedDto;
+    Object.defineProperty(PaginatedDto$, 'name', Object.assign(
+        Object.getOwnPropertyDescriptor(PaginatedDto$, 'name') as any,
+        { value: `Paginated${(<Function> dto).name}` }
+    ));
+
+    return PaginatedDto$<DtoType, InstanceType<PaginatorDto>>
 }
--- packages/uniform-data/dtos/uniform-data.dto.ts
+++ packages/uniform-data/dtos/uniform-data.dto.ts
@@ -2,17 +2,49 @@ import { ApiExtraModels, ApiProperty, getSchemaPath } from '@nestjs/swagger';
 import { ClassConstructor } from "@aequum/types";
 
 
-export function UniformDataDto<T extends ClassConstructor | [ ClassConstructor ]>(DataDto: T) {
-    const DataDtoArg = Array.isArray(DataDto) ? DataDto[0] : DataDto as Function;
+/**
+ * Factory class to create a DTO class for uniform data response,
+ * it means that the `DataDto` argument will be in `data` property.
+ *
+ * @typeParam DataDtoType - Take the type from the `DataDto`
+ * argument.
+ * @typeParam RealDataDtoType - Instance type from the `DataDto`
+ * argument if it is an array extract the type from the first
+ * element.
+ *
+ * @param DataDto Must be the DTO class or an array with the DTO
+ * class inside
+ */
+export function UniformDataDto<
+    DataDtoType extends ClassConstructor | [ ClassConstructor ],
+    RealDataDtoType = (
+        DataDtoType extends [ infer U extends ClassConstructor ]
+        ? InstanceType<U>[]
+        : (
+            DataDtoType extends ClassConstructor
+            ? InstanceType<DataDtoType>
+            : never
+        )
+    )
+>(DataDto: DataDtoType) {
+    const RealDataDto = (
+        Array.isArray(DataDto) ? DataDto[0] : DataDto
+    ) as Function;
 
-    @ApiExtraModels(DataDtoArg)
-    class UniformDataDto<U> {
+    @ApiExtraModels(RealDataDto)
+    class UniformDataDto$<U> {
         @ApiProperty({
             description: 'Data response',
-            $ref: getSchemaPath(DataDtoArg),
+            type: DataDto,
+            $ref: getSchemaPath(RealDataDto),
         } as any)
         data: U;
     }
 
-    return UniformDataDto<T>;
+    Object.defineProperty(UniformDataDto$, 'name', Object.assign(
+        Object.getOwnPropertyDescriptor(UniformDataDto$, 'name') as any,
+        { value: `${(<Function> DataDto).name.replace(/dto$/gi, '')}UniformDataDto` }
+    ));
+
+    return UniformDataDto$<RealDataDtoType>;
 }
